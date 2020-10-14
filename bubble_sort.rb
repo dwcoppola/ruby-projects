27cfb286f657f7b3e364761c52ce80c3eb974ca0
dwcoppola <dwcoppola@gmail.com>
2020-10-14 06:32:48 -0400

    Initial upload. Seems to work fine.

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(user_array)
+    swap = []
+    c = 0
+    for c in 0..(user_array.length ** 2)
+    i = 0
+        until i > user_array.length - 2
+                swap[i] = user_array[i]
+                swap[i + 1] = user_array[i + 1]
+                if swap[i] > swap[i + 1] 
+                user_array[i] = swap[i + 1]
+                user_array[i + 1] = swap[i]
+                else
+                end
+                i += 1
+        end
+    c += 1
+    end
+return user_array
+end
