fa417178a0c38efcb47b7e4e145778209ec16c2c
David <66121421+dwcoppola@users.noreply.github.com>
2020-10-20 11:45:07 -0400

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,19 @@
 def bubble_sort(user_array)
-    swap = []
-    c = 0
-    for c in 0..(user_array.length - 1)
+  swap = []
+  c = 0
+  for c in 0..(user_array.length - 1)
     i = 0
-        until i > user_array.length - 2
-                swap[i] = user_array[i]
-                swap[i + 1] = user_array[i + 1]
-                if swap[i] > swap[i + 1] 
-                user_array[i] = swap[i + 1]
-                user_array[i + 1] = swap[i]
-                else
-                end
-                i += 1
+    until i > user_array.length - 2
+      swap[i] = user_array[i]
+      swap[i + 1] = user_array[i + 1]
+        if swap[i] > swap[i + 1] 
+          user_array[i] = swap[i + 1]
+          user_array[i + 1] = swap[i]
+        else
         end
-    c += 1
+      i += 1
     end
-return user_array
+    c += 1
+  end
+  return user_array
 end
