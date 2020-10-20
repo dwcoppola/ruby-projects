915ffa74cbd58644d62ba6b2d44a105a46f8936a
David <66121421+dwcoppola@users.noreply.github.com>
2020-10-20 11:49:45 -0400

    Update stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,23 +1,22 @@
-# days = [17,3,6,9,15,8,6,1,10]
 def stockpicker(days)
-    buy_hash = {}
+  buy_hash = {}
+  buy_results = {}
+  max_buys = []
+  for j in 0..(days.length - 2)
+    i = j + 1
+      for i in i..(days.length - 1)
+        buy_results[i] = days[i] - days[j]
+      end
+    buy_hash[j] = buy_results
     buy_results = {}
-    max_buys = []
-    for j in 0..(days.length - 2)
-        i = j + 1
-        for i in i..(days.length - 1)
-            buy_results[i] = days[i] - days[j]
-        end
-        buy_hash[j] = buy_results
-        buy_results = {}
-    end
-    i = 0
-    until i > buy_hash.length - 1
-        buy_hash[i].select { 
-            |k, v| buy_results["#{i}, #{k}"] = v if v == buy_hash[i].values.max 
-        }
-        i += 1
-    end
-    buy_results = buy_results.select { |k, v| v == buy_results.values.max }
-    return buy_results
+  end
+  i = 0
+  until i > buy_hash.length - 1
+    buy_hash[i].select { 
+      |k, v| buy_results["#{i}, #{k}"] = v if v == buy_hash[i].values.max 
+    }
+    i += 1
+  end
+  buy_results = buy_results.select { |k, v| v == buy_results.values.max }
+  return buy_results
 end
